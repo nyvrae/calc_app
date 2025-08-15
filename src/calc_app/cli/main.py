fa7355f1da56d7a33fd22b7a1db88159f5151f7a
nyvrae <mex99anue@gmail.com>
2025-08-15 13:10:44 +0300

    feat: add calculations history

--- src/calc_app/cli/main.py
+++ src/calc_app/cli/main.py
@@ -14,11 +14,15 @@ def main():
         if not tokens:
             raise ValueError("Invalid expression")
         
-        print("Tokens:", [token for token in tokens])
+        print([token.value for token in tokens])
         
         result = evaluator.evaluate(tokens)
         
         print(result)
+        
+        with open('history.txt', 'a') as file:
+            file.write(f"{user_input} = {result}\n")
+            
     except Exception as e:
         print(f"Error: {str(e)}")
 
